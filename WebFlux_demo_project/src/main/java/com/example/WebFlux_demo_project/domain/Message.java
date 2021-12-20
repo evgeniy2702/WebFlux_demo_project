eb6c01601c8739affbf0589dbbb78a6ca4b7a0db
evgeniy2702 <evgeniy,zhurenko@gmail.com>
2021-12-20 11:01:45 +0200

    WebFluxDemoProject

---
+++ WebFlux_demo_project/src/main/java/com/example/WebFlux_demo_project/domain/Message.java
@@ -0,0 +1,14 @@
+package com.example.WebFlux_demo_project.domain;
+
+public class Message {
+
+    private String data;
+
+    public Message(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+}
